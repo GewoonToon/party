3e26e8430331b711c9ec94ef715a7eed86db6eff
GewoonToon <thundertonemail@gmail.com>
2021-02-16 12:33:29 +0100

    Venuelist, venuedetails, pay pages added Added fragments for header

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -3,6 +3,10 @@ package be.thomasmore.party.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
@@ -21,5 +25,23 @@ public class HomeController {
         mySpecialNumber = (int) (Math.random()*11+0);
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model){
+        model.addAttribute("date", LocalDate.now());
+        model.addAttribute("newDate", LocalDate.now().plusDays(30));
+        return "pay";
+    }
+
+    @GetMapping({"/venuedetails/{venueName}", "/venuedetails"})
+    public String venueDetails(Model model, @PathVariable(required = false) String venueName){
+        model.addAttribute("venueName", venueName!=null ? venueName : "--No venue chosen--");
+        return "venuedetails";
+    }
+
+    @GetMapping("/venuelist")
+    public String venueList(Model model){
+        return "venuelist";
     }
 }
