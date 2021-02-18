343216829e066a29c8467da647e39670ef6a43e5
ToonDubois <60922276+ToonDubois@users.noreply.github.com>
2021-02-18 15:02:49 +0100

    Iterators en conditional values added Request handler with index added

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -5,24 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
-    private int mySpecialNumber;
+    final int mySpecialNumber = 729;
+    private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+    private final DayOfWeek[] weekend = {DayOfWeek.SATURDAY, DayOfWeek.SUNDAY};
 
     @GetMapping({"/", "/home"})
     public String home(Model model){
-        mySpecialNumber = (int) (Math.random()*11+0);
+
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
 
     @GetMapping("/about")
     public String about(Model model){
-        mySpecialNumber = (int) (Math.random()*11+0);
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
@@ -31,17 +35,47 @@ public class HomeController {
     public String pay(Model model){
         model.addAttribute("date", LocalDate.now());
         model.addAttribute("newDate", LocalDate.now().plusDays(30));
+        model.addAttribute("weekend", isWeekend(weekend));
         return "pay";
     }
 
     @GetMapping({"/venuedetails/{venueName}", "/venuedetails"})
     public String venueDetails(Model model, @PathVariable(required = false) String venueName){
-        model.addAttribute("venueName", venueName!=null ? venueName : "--No venue chosen--");
+        model.addAttribute("venueName", venueName/*!=null ? venueName : "--No venue chosen--"*/);
         return "venuedetails";
     }
 
     @GetMapping("/venuelist")
     public String venueList(Model model){
+        model.addAttribute("venueNames", venueNames);
         return "venuelist";
+    }
+
+    public boolean isWeekend(DayOfWeek[] weekend){
+        for (DayOfWeek day : weekend){
+            if (LocalDate.now().getDayOfWeek().equals(day)){return true;}
+
+        }
+        return false;
+    }
+
+    @GetMapping({"/venuedetailsbyindex", "/venuedetailsbyindex/{optVenueIndex}"})
+    public String venueDetailsByIndex(Model model, @PathVariable Optional<Integer> optVenueIndex){
+        String venueName = null;
+        ArrayList<String> errors = new ArrayList<>();
+        int venueIndex=0;
+        if(optVenueIndex.isPresent()){
+            venueIndex = optVenueIndex.get();
+        }
+        else{errors.add("Geef een nummer");}
+        if (venueIndex<0 || venueIndex > venueNames.length-1){
+        errors.add("Geef een nummer dat bestaat");
+    }
+
+        if (errors.isEmpty()){
+        venueName = venueNames[venueIndex];}
+        model.addAttribute("errors", errors);
+        model.addAttribute("venueName",venueName);
+        return "venuedetails";
     }
 }
